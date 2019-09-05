88045a6c876bf237c93181de433464101af10336
Felipe Araújo Silva <felipesilva.it@gmail.com>
2019-09-05 09:29:28 -0300

    converting tabs to space (newly configured editor)

--- dummy_module.py
+++ dummy_module.py
@@ -7,10 +7,10 @@ import log
 here = os.path.dirname(__file__)
 
 logger = log.setup(log_name='dummy', log_to_console=True, 
-			log_to_file=True, dirname=os.path.join(here, 'logs'))
+            log_to_file=True, dirname=os.path.join(here, 'logs'))
 
 def foo():
-	logger.warning('some shit might be happen soon...')
-	logger.info('everything is normal')
+    logger.warning('some shit might be happen soon...')
+    logger.info('everything is normal')
 
 foo()
