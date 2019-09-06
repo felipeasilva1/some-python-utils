1b0cd380919b1ca23bf2a4d274cf817d3e2d4213
Felipe Araújo Silva <felipesilva.it@gmail.com>
2019-09-05 23:48:10 -0300

    handling no handler been passed

--- log.py
+++ log.py
@@ -34,7 +34,7 @@ def setup(log_name, log_to_console=True, log_to_file=False, dirname=None):
         console_handler = _configure_console_handler(logging.WARNING, formatter)
         handlers.append(console_handler)
 
-    if log_to_file:
+    elif log_to_file:
         if not dirname:
             raise AttributeError('if log_to_file is True, a dirname should be passed as well')
         try:
@@ -42,7 +42,10 @@ def setup(log_name, log_to_console=True, log_to_file=False, dirname=None):
                                 os.path.join(dirname, '%s.log' % log_name))
             handlers.append(file_handler)
         except IOError:
-            raise
+            raise  # re-raise _is_valid_location's exception
+            
+    else:
+        raise AttributeError('At least one handler must been choose.')
 
     logger = _configure_logger(log_name, logging.DEBUG, handlers)
 
